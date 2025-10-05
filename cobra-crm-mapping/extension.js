84518f6d3e96064b53c2479171178fc24d284d45
jbruch <bruch@jacob.de>
2025-10-05 02:13:42 +0200

    added extension logic

--- cobra-crm-mapping/extension.js
+++ cobra-crm-mapping/extension.js
@@ -1,36 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cobra-crm-mapping" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('cobra-crm-mapping.tableNameLogical', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// test-itmes
-		const items = ["TEXT1", "TEXT2"]
+	
+	console.log('extension is active');
+	vscode.window.showInformationMessage('Cobra-Mapping is active!');
+	
+	const Data = require('./data/cobra_crm_mapping.json');
+	const arrow = '\&#8594'
+	const cols = Data.map(table => {
+		return {
+			label: `${table.ColNameLogical} \u{2192} ${table.ColNameUser} | ${table.ColDescr}`, 
+			detail : `Tabelle: ${table.TableNameUser} | ${table.TableNameLogical}`
+		}
+	})
+	const disposable = vscode.commands.registerCommand(
+		'cobra-crm-mapping.queryCobraMapping', 
 		
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Cobra Mapping Extension is activated!');
+		function () {
+		const table = vscode.window.showQuickPick(cols, {
+			matchOnDetail: true,
+
+			
+		})
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {

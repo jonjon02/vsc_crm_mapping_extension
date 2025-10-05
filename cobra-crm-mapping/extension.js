cc536e41b3e9f627946c7cd1bdd58fddab42bf97
jbruch <bruch@jacob.de>
2025-10-05 14:39:01 +0200

    added table search

--- cobra-crm-mapping/extension.js
+++ cobra-crm-mapping/extension.js
@@ -3,30 +3,52 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	
+
 	console.log('extension is active');
 	vscode.window.showInformationMessage('Cobra-Mapping is active!');
 	
-	const Data = require('./data/cobra_crm_mapping.json');
+	var Data = require('./data/cobra_crm_mapping.json');
 	const arrow = '\&#8594'
-	const cols = Data.map(table => {
+	const cols = Data.sort().map(table => {
 		return {
-			label: `${table.ColNameLogical} \u{2192} ${table.ColNameUser} | ${table.ColDescr}`, 
-			detail : `Tabelle: ${table.TableNameUser} | ${table.TableNameLogical}`
+			label: `${table.ColNameLogical} \u{2192} ${table.ColNameUser}`, 
+			detail : `Tabelle: ${table.TableNameUser} | ${table.TableNameLogical}`,
+			description: table.ColDescr
 		}
 	})
-	const disposable = vscode.commands.registerCommand(
-		'cobra-crm-mapping.queryCobraMapping', 
+	
+	const test = vscode.commands.registerCommand(
+		'cobra-crm-mapping.queryCobraColumns', 
 		
 		function () {
 		const table = vscode.window.showQuickPick(cols, {
-			matchOnDetail: true,
-
-			
+			matchOnDetail: true
 		})
-	});
+	});	
 
-	context.subscriptions.push(disposable);
+	const queryTables  = vscode.commands.registerCommand(
+	'cobra-crm-mapping.queryCobraTables',
+
+	async function() {
+
+	const editor = vscode.window.activeTextEditor
+	const snippet = new vscode.SnippetString(
+		"SELECT ${cols.ColumnNameLogical} FROM `${tables.TableNameLogical}"
+	)
+
+		const tables = await vscode.window.showQuickPick(
+			[... (new Set(Data.map(item => 
+				`${item.TableNameLogical} \u{2192} ${item.TableNameUser}`)
+				)
+			)].sort()
+			)
+		
+		if (tables) {
+			vscode.window.showInformationMessage(`Sie haben ${tables} gewählt`)
+			//editor.insertSnippet(`${tables.T})
+		}
+		}
+	)
 }
 
 function deactivate() {}
